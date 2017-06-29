3394c45aadade1383f8af0c94dcc8cf35afe228d
goodatsports <goodatsports@gmail.com>
2017-06-29 14:22:03 -0700

    Bot will now join voice channel and play track on command!

--- deadboy.js
+++ deadboy.js
@@ -2,6 +2,7 @@ const discord = require('discord.js');
 const ClientOAuth2 = require('client-oauth2');
 const client = new discord.Client();
 const settings = require('./settings.json');
+var vol = 0.5;
 
 client.login(settings.token).then(
   (result) => {
@@ -14,4 +15,22 @@ client.login(settings.token).then(
 
 client.on('ready', () => {
   console.log('deadboy online~~');
+});
+
+
+
+client.on('message', message => {
+  if(message.content.startsWith('~dead')) {
+    console.log('MESSAGE RECEIVED');
+    message.channel.send('yo!');
+  }
+  if(message.content.startsWith('~play')) {
+    const channel = message.member.voiceChannel;
+    channel.join().then(conn => {
+      console.log("JOINED VOICE CHANNEL");
+      const dispatcher = conn.playFile('./DEAD.mp3', vol, 2);
+      isPlaying = true;
+    })
+    .catch(console.log);
+  }
 });
