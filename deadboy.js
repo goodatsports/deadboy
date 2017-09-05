b6a2dd04d224ee40c58e72fb2725ae7fc2392d34
goodatsports <goodatsports@gmail.com>
2017-09-04 20:58:58 -0700

    Moved song request structure to a separate .json file so bot is much more dry

--- deadboy.js
+++ deadboy.js
@@ -2,6 +2,7 @@ const discord = require('discord.js');
 const ClientOAuth2 = require('client-oauth2');
 const client = new discord.Client();
 const settings = require('./settings.json');
+const songs = settings.songs
 var vol = 0.5;
 
 //Bot login
@@ -26,25 +27,17 @@ client.on('message', message => {
   }
   // Music file (i.e. XO TOUR Llif3 by Lil Uzi Vert) will play
   // when '~dead' message is played in text chat
-  if(message.content.startsWith('~dead')) {
+
+
+  if(message.content in songs) {
     const channel = message.member.voiceChannel;
     channel.join().then(conn => {
       console.log("JOINED VOICE CHANNEL");
       // StreamOptions will play song from file, pass current volume setting,
       // and send voice packets twice to reduce packet loss
-      const dispatcher = conn.playFile('./DEAD.mp3', vol, 2);
+      const dispatcher = conn.playFile(songs[message.content], vol, 2);
       isPlaying = true;
     })
-    .catch(console.log);
-  }
-    // Play Pearl Jam's "Alive" if '~alive'
-    if(message.content.startsWith('~alive')) {
-      const channel = message.member.voiceChannel;
-      channel.join().then(conn => {
-        console.log("JOINED VOICE CHANNEL");
-        const dispatcher = conn.playFile('./ALIVE.mp3', vol - 0.4, 2);
-        isPlaying = true;
-      })
-      .catch(console.log);
+   .catch(console.log);
   }
 });
